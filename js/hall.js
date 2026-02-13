249a533420559da38d62a7592bdcd351eb3248e6
chemiology <172052088+chemiology@users.noreply.github.com>
2026-02-13 23:13:50 +0900

    Add files via upload

--- js/hall.js
+++ js/hall.js
@@ -12,3 +12,31 @@ async function loadHall() {
 }
 
 loadHall();
+
+async function loadHall() {
+  const params = new URLSearchParams(location.search);
+  const hallId = params.get("hall");
+
+  const res = await fetch("data/hall.json");
+  const data = await res.json();
+
+  const hall = data.halls.find(h => h.id === hallId);
+
+  document.getElementById("hallTitle").textContent =
+    hall ? hall.title : "Hall";
+
+  const list = document.getElementById("exhibitionList");
+
+  if (hall && hall.exhibitions.length > 0) {
+    hall.exhibitions.forEach(ex => {
+      const div = document.createElement("div");
+      div.textContent = ex;
+      div.style.margin = "20px";
+      div.style.fontSize = "20px";
+      list.appendChild(div);
+    });
+  }
+}
+
+loadHall();
+
